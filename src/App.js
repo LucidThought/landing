29496c1497cc7c6bc5637e2756a9f39195d05aaa
Andrew L <alata.it@gmail.com>
2020-05-22 13:50:45 -0600

    started work on top menu

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './ExyteLogo.png'
+import { Container } from 'react-bootstrap'
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <Container>
+      <header>
+        <img src={logo} alt="logo" />
+        <ul id="horizontal-menu">
+          <li><a href="./">Home</a></li>
+          <li><a href="./">Services</a></li>
+          <li><a href="./">Portfolio</a></li>
+          <li><a href="./">Team</a></li>
+          <li><a href="./">Blog</a></li>
+          <li><a href="./">Contacts</a></li>
+        </ul>
       </header>
-    </div>
-  );
+    </Container>
+  )
 }
 
 export default App;
